6ef374d8ef07e8db36ca826fc61dec4c20a02c81
Aquiles Mendez <aquilesmendez@gmail.com>
2023-05-26 09:20:37 -0400

    validacion de datos

--- app/src/main/java/com/example/proyectito/MainActivity.java
+++ app/src/main/java/com/example/proyectito/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -35,25 +36,48 @@ public class MainActivity extends AppCompatActivity {
                 String password = passwordEditText.getText().toString().trim();
 
                 if (!nombre.isEmpty() && !apellido.isEmpty() && !email.isEmpty() && !password.isEmpty()) {
-                    // Aquí puedes realizar la acción de registro con los datos ingresados
+                    // Validar que el nombre solo contenga letras
+                    if (nombre.matches("[a-zA-Z]+")) {
+                        // Validar que el apellido solo contenga letras
+                        if (apellido.matches("[a-zA-Z]+")) {
+                            // Aquí puedes realizar la acción de registro con los datos ingresados
+                            if (Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
 
-                    // Por ejemplo, mostrar un mensaje de éxito
-                    Toast.makeText(MainActivity.this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+                                if (password.length() >= 8 && password.matches(".*[A-Z].*") && password.matches(".*[!@#$%^&*()].*")) {
 
-                    // Abrir la actividad CompletadoActivity y pasar los datos
-                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                    intent.putExtra("nombre", nombre);
-                    intent.putExtra("apellido", apellido);
-                    intent.putExtra("email", email);
-                    intent.putExtra("password", password);
-                    startActivity(intent);
+                                    // Por ejemplo, mostrar un mensaje de éxito
+                            Toast.makeText(MainActivity.this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+
+                            // Abrir la actividad CompletadoActivity y pasar los datos
+                            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                            intent.putExtra("nombre", nombre);
+                            intent.putExtra("apellido", apellido);
+                            intent.putExtra("email", email);
+                            intent.putExtra("password", password);
+                            startActivity(intent);
+
+                                } else {
+                                    // Si la contraseña no cumple con los requisitos, muestra un mensaje de error
+                                    Toast.makeText(MainActivity.this, "La contraseña debe tener al menos 8 caracteres, una mayúscula y un símbolo", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                // Si el formato del email es incorrecto, muestra un mensaje de error
+                                Toast.makeText(MainActivity.this, "El formato del correo electrónico es incorrecto", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            // Si el apellido no contiene solo letras, muestra un mensaje de error
+                            Toast.makeText(MainActivity.this, "El apellido solo debe contener letras", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        // Si el nombre no contiene solo letras, muestra un mensaje de error
+                        Toast.makeText(MainActivity.this, "El nombre solo debe contener letras", Toast.LENGTH_SHORT).show();
+                    }
                 } else {
                     // Si alguno de los campos está vacío, muestra un mensaje de error
                     Toast.makeText(MainActivity.this, "Completa todos los campos", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
 
     }
 }
